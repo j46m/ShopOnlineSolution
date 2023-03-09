9cdf6a8ac07e1ac73850f095ecbbc4f11551e922
Juan <github@juangarcia.dev>
2023-03-09 12:01:08 -0600

    Adding domain entities

---
+++ ShopOnline.Domain/Entities/ProductCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopOnline.Domain.Entities
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ ShopOnline.Domain/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopOnline.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
