db8efb3a6d550fb793700a02a972d003b35b11f8
Juan <github@juangarcia.dev>
2023-03-09 19:30:11 -0600

    Cleaning products component to make it more readable

---
+++ ShopOnline.Web/Pages/DisplayProductsBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using ShopOnline.Data.DTO;
+
+namespace ShopOnline.Web.Pages;
+
+public class DisplayProductsBase : ComponentBase
+{
+    [Parameter]
+    public IEnumerable<ProductDto> Products { get; set; }
+}
